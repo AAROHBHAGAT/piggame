15ada72d0d1ce0b2ca733231318582553bc52c50
TheDudeWhoCodes <68582024+AAROHBHAGAT@users.noreply.github.com>
2021-11-21 12:45:25 -0800

    Added New Game Function
    Now able to Reset game once done without reloading.

--- assets/scripts/piggame.js
+++ assets/scripts/piggame.js
@@ -13,11 +13,20 @@ function show(){
     diceimg.style.display="block";
     roundscore=roundscore+dice;
     document.getElementById("roundscore-0").innerText=roundscore;  
-}
+};
 var scoreBox =0;
 var holdbtn =document.getElementById("hold");
 holdbtn.addEventListener("click", hold);
 function hold(){
     document.getElementById("scoreBox-0").innerText=roundscore;
     document.getElementById("roundscore-0").innerText=0;
+};
+var newGame =document.getElementById("newgame");
+newgame.addEventListener("click", reset);
+function reset(){
+    document.getElementById("scorebox-0").innerText=0;
+    document.getElementById("roundscore-0").innerText=0;
+    document.getElementById("scorebox-1").innerText=0;
+    document.getElementByID("roundscore-1").innertext=0
 }
+
